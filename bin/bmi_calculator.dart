dd3ffdec0fcd557d015eebc7193c65fceff26c8f
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-09-24 14:55:00 +0700

    history feature

--- bin/bmi_calculator.dart
+++ bin/bmi_calculator.dart
@@ -1,14 +1,3 @@
-void main() {
-  List<Map<String, dynamic>> riwayat = [];
-
-  // Simulasi beberapa input
-  hitungBMI(170, 65, riwayat);
-  hitungBMI(165, 75, riwayat);
-  hitungBMI(180, 85, riwayat);
-
-  // Tampilkan riwayat menggunakan perulangan
-  tampilkanRiwayat(riwayat);
-
   void hitungBMI(
     double tinggiCm,
     double beratKg,
@@ -39,5 +28,29 @@ void main() {
       'bmi': bmi,
       'kategori': kategori,
     });
+  }
+void main() {
+  List<Map<String, dynamic>> riwayat = [];
+
+  // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+
+  // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
+
+}
+
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  print('=== RIWAYAT PERHITUNGAN BMI ===');
+  for (int i = 0; i < riwayat.length; i++) {
+    var data = riwayat[i];
+    print('Data ${i + 1}:');
+    print('  Tinggi: ${data['tinggi']} cm');
+    print('  Berat: ${data['berat']} kg');
+    print('  BMI: ${data['bmi'].toStringAsFixed(2)}');
+    print('  Kategori: ${data['kategori']}');
+    print('---');
   }
 }
